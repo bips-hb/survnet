37d937375767fb2078d8cb210308f0e433b5a520
Marvin Wright <github@wrig.de>
2019-10-03 09:59:30 +0200

    add example

--- R/survnet.R
+++ R/survnet.R
@@ -24,6 +24,20 @@
 #' @param verbose Verbosity mode (0 = silent, 1 = progress bar, 2 = one line per epoch). 
 #' 
 #' @return Fitted model.
+#' @examples 
+#' library(survival)
+#' library(survnet)
+#' 
+#' # Survival data
+#' y <- veteran[, c(3, 4)]
+#' x <- veteran[, c(-2, -3, -4)]
+#' x <- data.frame(lapply(x, scale))
+#' breaks <- c(1, 50, 100, 200, 500, 1000)
+#' 
+#' # Fit simple model
+#' fit <- survnet(y = y, x = x, breaks = breaks)
+#' plot(fit$history)
+#' 
 #' @export
 #' @import survival keras
 #' @importFrom magrittr freduce
